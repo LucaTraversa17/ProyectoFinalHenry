26a12e6291c4b96bedb8ddfa2b7e5da72920ebbe
erredic <mateoerredic@gmail.com>
2024-06-17 11:11:33 -0500

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+function Mateo() {
+    return {
+        nombre: "Mateo",
+        edad: 20,
+        nacionalidad: "Colombia"
+    };
+}
+
+// Puedes exportar la función si es necesario
+module.exports = Mateo;
